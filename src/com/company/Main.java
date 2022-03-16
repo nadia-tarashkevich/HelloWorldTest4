eb4c06a56e28cd0c77be0f2cce2e6cb453e99ddc
Nadezhda Solovyeva <nadia.solovyeva@jetbrains.com>
2022-03-16 12:01:35 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a2;
+        int b2 = a2;
+        int MineHello = 2;
+        int it = MineHello;
+    }
+}
---
+++ src/com/company/ㅊㅍ.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class ㅊㅍ {
+    // some text
+    // cv
+    // ㅊㅍ ㅠㅜ ㅣㅏ ㅣㅏㅏㅏㅣㅏㅏㅏㅕㅏㅏㄱ드ㅔㅅ
+}
